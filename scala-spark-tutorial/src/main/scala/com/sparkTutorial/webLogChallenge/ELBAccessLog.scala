a55007e5ec8130aa930c8f1f5c535cfa2f7cdcdc
Timothée Nguyen <nguyeti.tr@gmail.com>
2018-12-25 13:11:09 -0500

    avg session time solution

--- scala-spark-tutorial/src/main/scala/com/sparkTutorial/webLogChallenge/ELBAccessLog.scala
+++ scala-spark-tutorial/src/main/scala/com/sparkTutorial/webLogChallenge/ELBAccessLog.scala
@@ -36,72 +36,51 @@ object ELBAccessLog {
   def parseFromLogLine(logline: String): Row = {
     val m = PATTERN.matcher(logline)
 
-    println(logline)
+//    println(logline)
     if (!m.find) {
       System.out.println("Cannot parse logline:  " + logline)
       logger.log(Level.ALL, "Cannot parse logline  " + logline)
       throw new RuntimeException("Error parsing logline")
     }
 
-    /*println(m.group(1))
-    println(m.group(2))
-    println(m.group(3))
-    println(m.group(4))
-    println("target ip " + m.group(5))
-    println("backend port " +m.group(6))
-    println("requestProcessingTime " +m.group(7))
-    println("backendProcessingTime " +m.group(8))
-    println("responseProcessingTime " +m.group(9))
-    println("elbStatusCode " +m.group(10))
-    println("backendStatusCode " +m.group(11))
-    println("receivedBytes " +m.group(12))
-    println("sentBytes " +m.group(13))
-    println("request " +m.group(14))
-    println("request " +m.group(15))
-    println("request " +m.group(16))
-    println("userAgent " +m.group(17))
-    println("sslCipher " +m.group(18))
-    println("sslProtocol " +m.group(19))*/
+//    println(m.group(1))
+//    println(m.group(2))
+//    println(m.group(3))
+//    println(m.group(4))
+//    println("target ip " + m.group(5))
+//    println("backend port " +m.group(6))
+//    println("requestProcessingTime " +m.group(7))
+//    println("backendProcessingTime " +m.group(8))
+//    println("responseProcessingTime " +m.group(9))
+//    println("elbStatusCode " +m.group(10))
+//    println("backendStatusCode " +m.group(11))
+//    println("receivedBytes " +m.group(12))
+//    println("sentBytes " +m.group(13))
+//    println("request " +m.group(14))
+//    println("request " +m.group(15))
+//    println("request " +m.group(16))
+//    println("userAgent " +m.group(17))
+//    println("sslCipher " +m.group(18))
+//    println("sslProtocol " +m.group(19))
     Row(
-      m.group(1),
-      m.group(2),
-      m.group(3),
-      m.group(4),
-      m.group(5),
-      m.group(6),
-      m.group(7),
-      m.group(8),
-      m.group(9),
-      m.group(10),
-      m.group(11),
-      m.group(12),
-      m.group(13),
-      m.group(14),
-      m.group(15),
-      m.group(16),
-      m.group(17),
-      m.group(18),
-      m.group(19))
+      m.group(1), // timestamp
+      m.group(2), // elb name
+      m.group(3), // client ip
+      m.group(4).toInt, // client port
+      if (m.group(5) == "") null else m.group(5), // target ip
+      if (m.group(6) == "") null else m.group(6), // target port
+      m.group(7).toFloat, // requestProcessingTime
+      m.group(8).toFloat, // backendProcessingTime
+      m.group(9).toFloat, // responseProcessingTime
+      m.group(10).toInt, // elbStatusCode
+      m.group(11).toInt, // backendStatusCode
+      m.group(12).toLong, // receivedBytes
+      m.group(13).toLong, // sentBytes
+      m.group(14), // request_verb
+      m.group(15), // request_url
+      m.group(16), // request_protocol
+      m.group(17), // user agent
+      m.group(18), // ssl Cipher
+      m.group(19)) // ssl protocol
   }
-}
-
-class ELBAccessLog(var dateTimeString: String,
-                   var elbName: String,
-                   var clientIpAddress: String,
-                   val clientPort: Int,
-                   var backendIpAddress: String,
-                   val backendPort: Int ,
-                   val requestProcessingTime: Float,
-                   val backendProcessingTime: Float,
-                   val responseProcessingTime: Float,
-                   val elbStatusCode: Float,
-                   val backendStatusCode: Float,
-                   val receivedBytes: Long,
-                   val sentBytes: Long,
-                   val request_verb: String,
-                   val request_url: String,
-                   val request_protocol: String,
-                   val userAgent: String,
-                   val sslCipher: String,
-                   val sslProtocol: String) extends Serializable {
 }
--- scala-spark-tutorial/src/main/scala/com/sparkTutorial/webLogChallenge/WebLogChallenge.scala
+++ scala-spark-tutorial/src/main/scala/com/sparkTutorial/webLogChallenge/WebLogChallenge.scala
@@ -22,95 +22,134 @@ object WebLogChallenge {
     val conf = new SparkConf().setAppName("collect").setMaster("local[*]")
     val sc = new SparkContext(conf)
     val sparkSession = SparkSession.builder().config(conf).getOrCreate()
-    val webLogRdd = sc.textFile("in/2015_07_22_mktplace_shop_web_log_sample.log")
-    println("Count: " + webLogRdd.count())
-    val webLogLines = webLogRdd.map(line => {
-      ELBAccessLog.parseFromLogLine(line)
-    }).cache()
-    // test
-    webLogLines.take(10000)
-    val df = sparkSession.createDataFrame(webLogLines,
-      StructType(
-        Seq(
-          StructField("timestamp", StringType,true),
-          StructField("elbName", StringType,true),
-          StructField("clientIpAddress", StringType,true),
-          StructField("clientPort", StringType,true),
-          StructField("backendIpAddress", StringType,true),
-          StructField("backendPort", StringType,true),
-          StructField("requestProcessingTime", StringType,true),
-          StructField("backendProcessingTime", StringType,true),
-          StructField("responseProcessingTime", StringType,true),
-          StructField("elbStatusCode", StringType,true),
-          StructField("backendStatusCode", StringType,true),
-          StructField("receivedBytes", StringType,true),
-          StructField("sentBytes", StringType,true),
-          StructField("request_verb", StringType,true),
-          StructField("request_url", StringType,true),
-          StructField("request_protocol", StringType,true),
-          StructField("userAgent", StringType,true),
-          StructField("sslCipher", StringType,true),
-          StructField("sslProtocol", StringType,true)
-        )
-      )
-    )
+        val webLogRdd = sc.textFile("in/2015_07_22_mktplace_shop_web_log_sample.log")
+//    val webLogRdd = sc.textFile("in/wrong_line.log")
+//    println("Count: " + webLogRdd.count())
+//    val webLogLines = webLogRdd.map(line => {
+//      ELBAccessLog.parseFromLogLine(line)
+//    }).cache()
+//    // test
+//    webLogLines.take(10000)
+//    val df = sparkSession.createDataFrame(webLogLines,
+//      StructType(
+//        Seq(
+//          StructField("timestamp_str", StringType, true),
+//          StructField("elbName", StringType, true),
+//          StructField("clientIpAddress", StringType, true),
+//          StructField("clientPort", IntegerType, true),
+//          StructField("backendIpAddress", StringType, true),
+//          StructField("backendPort", StringType, true),
+//          StructField("requestProcessingTime", FloatType, true),
+//          StructField("backendProcessingTime", FloatType, true),
+//          StructField("responseProcessingTime", FloatType, true),
+//          StructField("elbStatusCode", IntegerType, true),
+//          StructField("backendStatusCode", IntegerType, true),
+//          StructField("receivedBytes", LongType, true),
+//          StructField("sentBytes", LongType, true),
+//          StructField("request_verb", StringType, true),
+//          StructField("request_url", StringType, true),
+//          StructField("request_protocol", StringType, true),
+//          StructField("userAgent", StringType, true),
+//          StructField("sslCipher", StringType, true),
+//          StructField("sslProtocol", StringType, true)
+//        )
+//      )
+//    ).cache()
+//
+//    df
+//      .select("timestamp_str", "clientIpAddress", "request_url")
+//      .withColumn("timestamp", df("timestamp_str").cast(TimestampType))
+//      .orderBy("clientIpAddress","timestamp").write.mode(SaveMode.Append).format("parquet").save("out/userLog.parquet")
+    val parquetFileDF = sparkSession.read.parquet("out/userLog.parquet")
+    parquetFileDF.show(100, false)
+    val client = parquetFileDF
+        .select("timestamp", "clientIpAddress", "request_url")
+      .withColumn("timestamp_cast", parquetFileDF("timestamp").cast(TimestampType))
+//        .filter(parquetFileDF("clientIpAddress") === "123.242.248.130" )
+    client.show(100, false)
+    val w = Window.partitionBy("clientIpAddress").orderBy("timestamp_cast")
+    val lagDF = client.withColumn("lag", lag("timestamp_cast",1).over(w))
+    lagDF
+        .select("timestamp_cast", "lag", "clientIpAddress", "request_url")
+        .show(100000, false)
 
-    df.createOrReplaceTempView("userLog")
-    val selectDF = sparkSession.sql("SELECT * FROM userLog ORDER BY timestamp DESC")
-    selectDF.show(20, 0)
-   /* val format = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSSSSS'Z'")
-    val webLogLinesPair = webLogRdd.map(line => {
-      val splits = line.split("\\s")
-      Row(
-        splits(2).split(":")(0),
-        new Timestamp(format.parse(splits(0)).getTime),
-        splits(3).split(":")(0)
-      )
-    })
+    val diff_in_sec_DF = lagDF.withColumn("diff_in_secs", unix_timestamp(col("timestamp_cast")) - unix_timestamp(col("lag")))
+    diff_in_sec_DF.
+      show(20, 0)
 
-    val df = sparkSession.createDataFrame(webLogLinesPair,
-        StructType(Seq(StructField("userId", StringType,true),
-                       StructField("timestamp", TimestampType,true),
-                       StructField("uri", StringType,true))))
+    val new_session_flag_DF = diff_in_sec_DF.withColumn("new_session", when(diff_in_sec_DF("diff_in_secs") >= 30*60, 1).otherwise(0))
+     new_session_flag_DF.show(10000, 0)
 
-    //df.withColumn("timestamp", "timestamp".cast(LongType).cast(TimestampType))
-    //println(df.count())
-    //df.select("userId", "timestamp").write.mode(SaveMode.Append).format("parquet").save("out/userLog.parquet")
-    //val parquetFileDF = sparkSession.read.parquet("out/userLog.parquet")
+    val wSpec = Window.partitionBy("clientIpAddress").orderBy("timestamp_cast")
+    val df4 = new_session_flag_DF
+      .withColumn("session_id", concat(concat(new_session_flag_DF("clientIpAddress"),lit("_")),(sum("new_session")).over(wSpec)))
+      .select("session_id", "timestamp_cast", "clientIpAddress", "request_url", "diff_in_secs")
 
-    // Parquet files can also be used to create a temporary view and then used in SQL statements
-    df.createOrReplaceTempView("userLog")
-    val namesDF = sparkSession.sql("SELECT userId, timestamp, targetIp, lag(timestamp) OVER (PARTITION BY userId ORDER BY timestamp) as lag_timestamp FROM (SELECT * FROM userLog WHERE userId ='123.242.248.130') ORDER BY timestamp")
-    //namesDF.withColumn("difference", namesDF."timestamp")
-    //namesDF.show(20, 0)
-    val df2 = namesDF.withColumn("diff_in_secs", unix_timestamp(col("timestamp")) - unix_timestamp(col("lag_timestamp")))
-    //df2.show(20, 0)
+    df4.show(1000, 0)
 
-    val df3 = df2.withColumn("new_session", when(df2("diff_in_secs") >= 30*60, 1).otherwise(0))
-   // df3.show(100, 0)
-   // val hitByUsers = webLogLinesPair.groupByKey()
-    val wSpec = Window.partitionBy("userId").orderBy("timestamp")
-    val df4 = df3.withColumn("session_id", concat(concat(df3("userId"),lit("_")),(sum("new_session")).over(wSpec)))
-    //df4.show(100, 0)
-   // df4.select("userId", "timestamp", "targetIp","session_id").write.mode(SaveMode.Append).format("parquet").save("out/logs.parquet")
-    val parquetFileDF = sparkSession.read.parquet("out/logs.parquet")
-    // distinct ip
-    val wSpec2 = Window.partitionBy("targetIp").orderBy("timestamp")
-    val resDF = parquetFileDF
-      .filter(parquetFileDF("session_id") === "123.242.248.130_1" || parquetFileDF("session_id") === "123.242.248.130_2")
-      .withColumn("targetIpCount", row_number().over(wSpec2))
-    val resDF2= resDF
-        .withColumn("targetCountIp", when(resDF("targetIpCount") < 2,1).otherwise(0))
-    //parquetFileDF.createOrReplaceTempView("logs")
-    //val resDF = sparkSession.sql("SELECT *, row_number()  as ff from logs where session_id ='123.242.248.130_1' ")
-          resDF2.show()
-    val wSpec3 = Window
-      .partitionBy("targetIp")
-    val tt = resDF2
-      .select(resDF2("session_id"),resDF2("targetIp"), sum(resDF2("targetCountIp")).over(wSpec3).alias("test")).distinct()
-    tt.show()
-    val ttt = tt.groupBy("session_id").sum("test")
-    ttt.show()*/
+    val wSpec2 = Window.partitionBy("session_id")
+    val avg_time_by_session = df4.withColumn("total_session_time", sum("diff_in_secs").over(wSpec2))
+    avg_time_by_session.show(10000, false)
+
+    val avg_session_time = avg_time_by_session.select("session_id", "total_session_time").distinct()
+      avg_session_time.show(10, 0)
+    val res = avg_session_time.select(avg("total_session_time"))
+    res.show()
+//    val selectDF = sparkSession.sql("SELECT * FROM userLog ORDER BY timestamp DESC")
+//    selectDF.show(20, 0)
+    /* val format = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSSSSS'Z'")
+     val webLogLinesPair = webLogRdd.map(line => {
+       val splits = line.split("\\s")
+       Row(
+         splits(2).split(":")(0),
+         new Timestamp(format.parse(splits(0)).getTime),
+         splits(3).split(":")(0)
+       )
+     })
+
+     val df = sparkSession.createDataFrame(webLogLinesPair,
+         StructType(Seq(StructField("userId", StringType,true),
+                        StructField("timestamp", TimestampType,true),
+                        StructField("uri", StringType,true))))
+
+     //df.withColumn("timestamp", "timestamp".cast(LongType).cast(TimestampType))
+     //println(df.count())
+     //df.select("userId", "timestamp").write.mode(SaveMode.Append).format("parquet").save("out/userLog.parquet")
+     //val parquetFileDF = sparkSession.read.parquet("out/userLog.parquet")
+
+     // Parquet files can also be used to create a temporary view and then used in SQL statements
+     df.createOrReplaceTempView("userLog")
+     val namesDF = sparkSession.sql("SELECT userId, timestamp, targetIp, lag(timestamp) OVER (PARTITION BY userId ORDER BY timestamp) as lag_timestamp FROM (SELECT * FROM userLog WHERE userId ='123.242.248.130') ORDER BY timestamp")
+     //namesDF.withColumn("difference", namesDF."timestamp")
+     //namesDF.show(20, 0)
+     val df2 = namesDF.withColumn("diff_in_secs", unix_timestamp(col("timestamp")) - unix_timestamp(col("lag_timestamp")))
+     //df2.show(20, 0)
+
+     val df3 = df2.withColumn("new_session", when(df2("diff_in_secs") >= 30*60, 1).otherwise(0))
+    // df3.show(100, 0)
+    // val hitByUsers = webLogLinesPair.groupByKey()
+     val wSpec = Window.partitionBy("userId").orderBy("timestamp")
+     val df4 = df3.withColumn("session_id", concat(concat(df3("userId"),lit("_")),(sum("new_session")).over(wSpec)))
+     //df4.show(100, 0)
+    // df4.select("userId", "timestamp", "targetIp","session_id").write.mode(SaveMode.Append).format("parquet").save("out/logs.parquet")
+     val parquetFileDF = sparkSession.read.parquet("out/logs.parquet")
+     // distinct ip
+     val wSpec2 = Window.partitionBy("targetIp").orderBy("timestamp")
+     val resDF = parquetFileDF
+       .filter(parquetFileDF("session_id") === "123.242.248.130_1" || parquetFileDF("session_id") === "123.242.248.130_2")
+       .withColumn("targetIpCount", row_number().over(wSpec2))
+     val resDF2= resDF
+         .withColumn("targetCountIp", when(resDF("targetIpCount") < 2,1).otherwise(0))
+     //parquetFileDF.createOrReplaceTempView("logs")
+     //val resDF = sparkSession.sql("SELECT *, row_number()  as ff from logs where session_id ='123.242.248.130_1' ")
+           resDF2.show()
+     val wSpec3 = Window
+       .partitionBy("targetIp")
+     val tt = resDF2
+       .select(resDF2("session_id"),resDF2("targetIp"), sum(resDF2("targetCountIp")).over(wSpec3).alias("test")).distinct()
+     tt.show()
+     val ttt = tt.groupBy("session_id").sum("test")
+     ttt.show()*/
 
 
     //val sample = hitByUsers.sample(withReplacement = true, fraction = 0.005)
